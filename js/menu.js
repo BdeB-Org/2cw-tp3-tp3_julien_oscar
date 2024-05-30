d1907234568dc2dc36c4babaf7e9124a031de3f5
Tan-Oscar <2358933@bdeb.qc.ca>
2024-05-30 15:59:32 -0400

    ajouter tableau produit sur l'index.html et ajouter un image de fond

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,3 @@
-
-
-
 const style = document.createElement("style")
 style.textContent = "ul li { display: flex; padding-top:2em; margin-left:-2em; align-items: center; justify-content: center;}"
 document.head.appendChild(style)
@@ -19,14 +16,20 @@ function append(parent, el) {
 
 
 const menu_ul = document.getElementById("menu");
-const elemH1 = document.getElementById("h1");
+var elemH1 = document.getElementById("h1");
 
 
 const url = "http://localhost:8080/ords/tp3/menu/";
 
+const produit_ul = document.getElementById("produit");
+var elemH2 = document.getElementById("p");
+
+
+const link = "http://localhost:8080/ords/tp3/produit/";
+
 
 elemH1.innerHTML = "Menu";
-total = 0
+elemH2.innerHTML = "Produit";
 
 fetch(url)
     .then((resp) => resp.json())
@@ -36,11 +39,38 @@ fetch(url)
         return menu.map(function (menu) {
             let li = createNode("li"),
                 span = createNode("span");
+            var a = document.createElement("a");
+            a.setAttribute('href', "#affichage_produit")
 
             span.innerHTML = `${menu.id_menu}. ${menu.type}`;
 
+            if (id_menu = 2) {
+                span.appendChild(a)
+            }
+
             append(li, span);
             append(menu_ul, li);
+        });
+
+    })
+
+    .catch(function (error) {
+        console.log(JSON.stringify(error));
+    });
+
+fetch(link)
+    .then((resp) => resp.json())
+    .then(function (data) {
+        let produit = data.items;
+
+        return produit.map(function (produit) {
+            let li = createNode("li"),
+                span = createNode("span");
+
+            span.innerHTML = `${produit.id_produit}. ${produit.nom}, Description: ${produit.description_produit}, Aliments: ${produit.aliments}, Prix: ${produit.prix}$`;
+
+            append(li, span);
+            append(produit_ul, li);
         });
 
     })
--- js/syntaxe.js
+++ js/syntaxe.js
@@ -1,17 +1,10 @@
-fetch(url) // Envoie une requête HTTP GET à l'URL spécifiée
+fetch(url, link)
     .then((response) => {
-        // Fonction anonyme exécutée lorsque la réponse est reçue
-        // Ici, vous pouvez effectuer des vérifications sur la réponse, comme vérifier le statut HTTP, etc.
-        // Vous pouvez également retourner la réponse telle quelle ou la transformer en un autre format
-        return response.json(); // Exemple: conversion de la réponse en JSON
+        return response.json();
     })
     .then((data) => {
-        // Fonction anonyme exécutée lorsque les données sont disponibles
-        // Ici, vous pouvez travailler avec les données récupérées
-        console.log(data); // Exemple: affichage des données dans la console
+        console.log(data);
     })
     .catch((error) => {
-        // Fonction anonyme exécutée en cas d'erreur lors de la récupération des données
-        // Ici, vous pouvez gérer l'erreur, par exemple, afficher un message d'erreur à l'utilisateur ou enregistrer l'erreur dans la console
-        console.error('Error:', error); // Exemple: affichage de l'erreur dans la console
+        console.error('Error:', error);
     });
